149d99af0db81076a09e9bd1524f578b66e8bc19
tumblegamer <tumble@tumblegamer.tk>
2015-11-05 19:00:50 +0000

    unzip update added

--- update.bat
+++ update.bat
@@ -2,5 +2,10 @@
 set URL=https://github.com/tumblegamer/tumble-hub/archive/master.zip
 set BIN=%~p0bin
 
-%BIN%\wget -O=update.zip %URL%
+%BIN%\wget --no-check-certificate -Oupdate.zip %URL%
+set URL=
+%BIN%\unzip -uo update
+del update.zip
+move /y %~p0tumble-hub-master\* %~p0
+rmdir tumble-hub-master
 pause
